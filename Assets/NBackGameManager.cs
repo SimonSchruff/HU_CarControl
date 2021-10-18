0ba8b179c92b624d4aa73bf3765f863f3a90a5aa
Simon - MacBook <simon.schruff@gmail.com>
2021-10-18 21:35:13 +0200

    Added NBackGameManager; More UI;

--- Assets/NBackGameManager.cs
+++ Assets/NBackGameManager.cs
@@ -14,6 +14,13 @@ public class NBackGameManager : MonoBehaviour
         gameOver
     }
     public GameState gameState; 
+
+    //Letters are directly assigned to List in inspector
+    public List<string> usedLetters = new List<string>(); 
+
+    [Header("UI References")]
+    public GameObject InstructionObjects; 
+    public GameObject RunningGameObjects; 
     
     void Awake()
     {
@@ -28,11 +35,49 @@ public class NBackGameManager : MonoBehaviour
 
     void Start()
     {
+    
         gameState = GameState.instructions; 
     }
 
     void Update()
     {
+        switch(gameState)
+        {
+            case GameState.instructions: 
+            //Show Instructions UI
+            InstructionObjects.SetActive(true); 
+            RunningGameObjects.SetActive(false); 
+            break; 
+
+            case GameState.running: 
+            InstructionObjects.SetActive(false); 
+            RunningGameObjects.SetActive(true); 
+            //Disable Instructions UI 
+            //Enable Game UI 
+            //Start Game Logic
+            break; 
+
+            case GameState.gameOver: 
+            //Show Restart Button ? 
+            //Save Score etc. 
+            break; 
+        }
         
     }
+
+    public void ChangeGameState(int i)
+    {
+        //Assign int in button etc. 
+        switch(i)
+        {
+            case 0 : 
+            gameState = GameState.instructions; 
+            break; 
+            case 1 : 
+            gameState = GameState.running; 
+            break; 
+        }
+    }
+
+    
 }
