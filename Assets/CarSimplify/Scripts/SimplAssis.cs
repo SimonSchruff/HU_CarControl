d6cb398b1bfa1a4bfb3353386038dd64790acab2
MarkiGa <73349060+MarkiGa@users.noreply.github.com>
2021-11-22 19:35:03 +0100

    Assistance working

--- Assets/CarSimplify/Scripts/SimplAssis.cs
+++ Assets/CarSimplify/Scripts/SimplAssis.cs
@@ -13,12 +13,15 @@ public class SimplAssis : MonoBehaviour
     [SerializeField] Crosses[] crossHightlightPair_2;
     [SerializeField] Crosses[] crossHightlightPair_3;
 
+    float autoUpdateTimer = 0;
+
     public static SimplAssis assi;
     public enum assiState
     {
         none,
         areaHelp,
-        specificHelp
+        specificHelp,
+        auto
     }
 
     private void Awake()
@@ -31,21 +34,17 @@ public class SimplAssis : MonoBehaviour
 
     public void ChangeAssitance (assiState changeStateTo)
     {
-        foreach (var sprite in highlightSprites)
-        {
-            sprite.gameObject.SetActive(false);
-        }
-        foreach (var cross in Control.con.crossesRefs)
-        {
-            cross.SetHighlighted(false);
-        }
+        ResetHighlightAreas();
+
+        ResetHighlightedCrosses();
 
 
         actualAssistance = changeStateTo;
+
+        UpdateAssistance();
     }
 
-
-    private void Update()
+    public void UpdateAssistance ()
     {
         switch (actualAssistance)
         {
@@ -57,9 +56,67 @@ public class SimplAssis : MonoBehaviour
             case assiState.areaHelp:
                 break;
             case assiState.specificHelp:
-                break;
+                {
+                    ResetHighlightedCrosses();
+
+                    int counter = 0;
+                    foreach (var cross in SearchForNextRecommendations())
+                    {
+                        counter++;
+                        cross.SetHighlighted(counter, true);
+                    }
+
+                    break;
+                }
+            case assiState.auto:
+                {
+                    StartCoroutine(DelayAutoUpdate());
+
+                    break;
+                }
             default:
                 break;
+        }
+    }
+    IEnumerator DelayAutoUpdate ()
+    {
+        yield return new WaitForSeconds(1f);
+
+        foreach (var cross in SearchForNextRecommendations())
+        {
+            cross.CrossClicked();
+        }
+    }
+    public void SetUpdateTimer()
+    {
+        autoUpdateTimer = .5f;
+    }
+
+    private void Update()
+    {
+        if(autoUpdateTimer>0)
+        {
+            autoUpdateTimer -= Time.deltaTime;
+
+            if (autoUpdateTimer<= 0)
+            {
+                UpdateAssistance();
+            }
+        }
+    }
+
+    void ResetHighlightedCrosses ()
+    {
+        foreach (var cross in Control.con.crossesRefs)
+        {
+            cross.SetHighlighted();
+        }
+    }
+    void ResetHighlightAreas()
+    {
+        foreach (var sprite in highlightSprites)
+        {
+            sprite.gameObject.SetActive(false);
         }
     }
 
@@ -67,6 +124,9 @@ public class SimplAssis : MonoBehaviour
     {
         List<Crosses> crosList = new List<Crosses>();
         Crosses[] crosRef = Control.con.crossesRefs;
+
+        List<Crosses> excludeCrosses = new List<Crosses>();
+
 
         for (int i = 0; i < 6; i++)
         {
@@ -78,13 +138,42 @@ public class SimplAssis : MonoBehaviour
                     {
                         if(cross.actualState != (vec.y==0))
                         {
-                            bool possible = false;
+                            bool possible = true;
                             foreach (var usedCrosses in crosList)
                             {
-                              //  if((cross.actualState? cross.previousCrossH: cross.previousCrossV).
+                                foreach(var prevCross in (cross.actualState? cross.previousCrossH: cross.previousCrossV))
+                                {
+                                    if(prevCross == usedCrosses)
+                                    {
+                                        possible = false;
+                                        break;
+                                    }
+                                }
+                                if (possible == false)
+                                {
+                                    break;
+                                }
+                            }
+                            if (possible)
+                            {
+                                foreach (var exlCross in excludeCrosses)
+                                {
+                                    if (cross == exlCross)
+                                    {
+                                        possible = false;
+                                        break;
+                                    }
+                                }
+                                if (possible)
+                                {
+                                    crosList.Add(cross);
+                                }
                             }
                         }
-
+                        else    //Exclude when cross needs to be crossed and cross standing right
+                        {
+                            excludeCrosses.Add(cross);
+                        }
                     }
                 }
             }
